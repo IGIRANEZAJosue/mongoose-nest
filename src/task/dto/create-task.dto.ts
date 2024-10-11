8f172a42202dbac1dbffc874e98fef1ba3660ecc
Josue Igiraneza <igiranezaj28@gmail.com>
2024-10-11 11:49:46 +0200

    add create task dto

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,1 +1,13 @@
-export class CreateTaskDto {}
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateTaskDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsString()
+  description?: string;
+
+  @IsBoolean()
+  completed: boolean;
+}
