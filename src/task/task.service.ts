1fd781d5813a9b450ba7d42aa90c55c4dd42912c
Josue Igiraneza <igiranezaj28@gmail.com>
2024-10-11 11:40:52 +0200

    Injecting mongoose model into services and models

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Task } from './schemas/task.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class TaskService {
+  constructor(@InjectModel(Task.name) private userModel: Model<Task>) {}
+
   create(createTaskDto: CreateTaskDto) {
     return 'This action adds a new task';
   }
