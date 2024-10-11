c36217d0addfc2182a3b442e8cae655d0e6a24cc
Josue Igiraneza <igiranezaj28@gmail.com>
2024-10-11 09:22:02 +0200

    feat: create task schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TaskModule } from './task/task.module';
+import { TaskModule } from './task/tasks.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [TaskModule],
+  imports: [
+    TaskModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://igiranezaj28:Cmnder%2F09@nodejstut.djz3xrv.mongodb.net/taskApp?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
